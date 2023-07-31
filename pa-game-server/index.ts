8349f5047f4d39e9b133d19c2b576c66ccf00474
Damjan Kovačič <letecnez@gmail.com>
2023-07-31 11:31:31 +0200

    Some websocket boilerplate

--- pa-game-server/index.ts
+++ pa-game-server/index.ts
@@ -2,15 +2,39 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import webSocket from 'ws';
+import { uuid } from 'uuidv4';
 
 const port = 3000;
 const app = express();
+const wss = new webSocket.Server({ port: 6661 });
+
+const clients = new Map();
+
+wss.on('connection', (ws) => {
+	const id = uuid();
+	const color = Math.floor(Math.random() * 360);
+	const metadata = { id, color };
+	clients.set(ws, metadata);
+
+	ws.on('message', (data) => {
+		const message = JSON.parse(data.toString());
+		const metadata = clients.get(ws);
+
+		console.log(message, metadata);
+	});
+
+	ws.on('close', () => {
+		clients.delete(ws);
+	});
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.json({ status: 'ok' });
+	res.json({ status: 'ok' });
 });
 
 const server = http.createServer(app);
